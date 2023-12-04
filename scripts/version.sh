4245e0ee8e5ad182222acf23388bd0204fa40160
yoza <andreaiossa97@gmail.com>
2023-12-04 14:30:17 +0100

    added bump control in version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,16 +1,32 @@
 function do_it() {
+  MODES_ARRAY=("patch" "minor" "major")
   CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   CHANGESET_FOLDER=$CURRENT_DIR/../changesets
-  BUMP=no-bumb
+  BUMP=-1
 
 
-  echo "Preparing to version package"
-  for file in $CHANGESET_FOLDER/*.txt
-  do
-      while read mode timestamp message; do
-          echo "$mode - $timestamp - $message"
-      done < file
+  echo "Preparing to version package: "
+  for file in "$CHANGESET_FOLDER/*.txt"; do
+    while read mode timestamp message; do
+      for i in "${!MODES_ARRAY[@]}"; do
+        [[ "${MODES_ARRAY[$i]}" = "$mode" ]] && break
+      done
+      
+      if [ $i > $BUMP ]; then
+          BUMP=$i
+      fi
+    done < $file
   done
+
+  if [ $BUMP == 1 ]; then
+    echo "No changesets found. Aborting version"
+    exit 0
+  fi
+
+  echo mode selected: "${MODES_ARRAY[$BUMP]}"
+
+
+
 }
 
 do_it "$@"
