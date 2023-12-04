3d63ed5f9b4a984690d0b1fd98a83b6fa4c8f0e7
yoza <andreaiossa97@gmail.com>
2023-12-04 15:18:21 +0100

    modified version file

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,31 +2,44 @@ function do_it() {
   MODES_ARRAY=("patch" "minor" "major")
   CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   CHANGESET_FOLDER=$CURRENT_DIR/../changesets
+  META_FILE=$CHANGESET_FOLDER/meta.yaml
   BUMP=-1
 
 
   echo "Preparing to version package: "
-  for file in "$CHANGESET_FOLDER/*.txt"; do
+  for file in $CHANGESET_FOLDER/*.txt; do
     while read mode timestamp message; do
       for i in "${!MODES_ARRAY[@]}"; do
         [[ "${MODES_ARRAY[$i]}" = "$mode" ]] && break
       done
       
-      if [ $i > $BUMP ]; then
+      if [ $i -gt $BUMP ]; then
           BUMP=$i
       fi
     done < $file
   done
 
-  if [ $BUMP == 1 ]; then
+  if [ $BUMP -lt 0 ]; then
     echo "No changesets found. Aborting version"
     exit 0
   fi
 
-  echo mode selected: "${MODES_ARRAY[$BUMP]}"
+  local MODE
+  local NEW_VERSION
+  local OLD_VALUE
+  local NEW_VALUE
 
+  MODE="${MODES_ARRAY[$BUMP]}"
+  echo mode selected: $MODE
+  
+  OLD_PATCH=$(yq .$MODE $META_FILE)
+  NEW_VALUE=$(( $OLD_VALUE + 1 ))
+  yq -i ".$MODE = $NEW_VALUE" $META_FILE
 
+  NEW_VERSION=$(yq .major $META_FILE).$(yq .minor $META_FILE).$(yq .patch $META_FILE)
+  echo $NEW_VERSION
 
+  # yq -i ".version = " $META_FILE
 }
 
 do_it "$@"
